929b8f72c8e91f23cc5b884b58dd7d9d36e4f921
root <root@DESKTOP-5NIG2QK>
2024-04-22 19:05:01 +0200

    remove unnecessary, unused files in the repo; move plugin to subfolder

--- utils.py
+++
@@ -1,6 +0,0 @@
-# Extract the function name from the custom function string
-def extract_function_name(custom_function_str: str):
-    lines = custom_function_str.splitlines()
-    for line in lines:
-        if line.strip().startswith("def "):
-            return line.split()[1].split("(")[0]
